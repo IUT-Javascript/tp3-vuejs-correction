ecb69e81f8177020648b5d51bf546b19a026e277
Alexandre Ruiz <alexandre.ruiz@apside.com>
2025-02-10 21:15:22 +0100

    Update style

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import { ref } from 'vue';
 import ArticleForm from './components/ArticleForm.vue';
 import ArticleList from './components/ArticleList.vue';
 
-const ARTICLES_API = 'https://6797ded3c2c861de0c6e4858.mockapi.io/articles';
+import { ARTICLES_API } from './const/config.js';
 
 const articles = ref([]);
 getArticlesFromApi();
@@ -35,8 +35,7 @@ function postArticleToApi(article){
       "Content-Type": "application/json",
       },
       body: JSON.stringify(article)
-  }).then();
-    
+  }).then();    
 }
 
 </script>
@@ -47,13 +46,13 @@ function postArticleToApi(article){
   </header>
 
   <main>
-    <section class="col col-4">
+    <section class="col col-4 p-1 v-align-top">
       <h2 class="text-center">Add article</h2>
 
       <ArticleForm @add-article="addArticle"/>
     </section>
 
-    <section class="col col-8">
+    <section class="col col-8 v-align-top">
       <h2 class="text-center">Article list</h2>
 
       <ArticleList :articles="articles"/>
